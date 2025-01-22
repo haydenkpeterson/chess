8e7738e1c2eee4774a4eae3e09d4a457cfb6ede2
hkp03 <haydenkpeterson@gmail.com>
2025-01-21 19:48:07 -0700

    Passed off first two tests of KingMovesCalculator.java

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -25,7 +25,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        board[position.getRow()-1][position.getColumn()-1] = piece;
+        board[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class KingMovesCalculator {
-    Collection<ChessMove> validMoves;
+    Collection<ChessMove> validMoves = new ArrayList<>();
+
     /**
      * Function for King moves
      */
+
     public Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece king_piece = board.getPiece(myPosition);
         for(int i = -1; i < 2; i++) {
             for(int j = -1; j < 2; j++) {
-                if(i != 0 && j != 0) {
+                if(!(i == 0 && j == 0)) {
                     ChessPosition newPosition = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() + j);
-                    if((newPosition.getRow() < 0 || newPosition.getRow() > 7) || (newPosition.getColumn() < 0 || newPosition.getColumn() > 7)){
-                        continue;
-                    }
-                    else {
+                    if((newPosition.getRow() >= 0 || newPosition.getRow() < 7) || (newPosition.getColumn() >= 0 || newPosition.getColumn() < 7)){
                         if (board.getPiece(newPosition) == null) {
                             ChessMove move = new ChessMove(myPosition, newPosition, null);
                             validMoves.add(move);
-                        } else {
+                        }
+                        else {
                             if (board.getPiece(newPosition).getTeamColor() != king_piece.getTeamColor()) {
                                 ChessMove move = new ChessMove(myPosition, newPosition, null);
                                 validMoves.add(move);
@@ -31,6 +31,6 @@ public class KingMovesCalculator {
                 }
             }
         }
-        return this.validMoves;
+        return validMoves;
     }
 }
