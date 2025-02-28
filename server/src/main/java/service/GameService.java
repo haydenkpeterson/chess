8936d88ae550ef0f63d0a1f1c332b36fe607cd71
hkp03 <haydenkpeterson@gmail.com>
2025-02-28 12:31:34 -0700

    started adding functionality to GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,11 +1,15 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
 import model.AuthData;
+import model.GameData;
 import model.UserData;
 import dataaccess.UserDAO;
 import dataaccess.AuthDAO;
+
+import java.util.ArrayList;
 
 public class GameService {
     private final UserDAO userDao;
@@ -17,6 +21,25 @@ public class GameService {
         this.userDao = userDao;
         this.authDao = authDao;
         this.gameDAO = gameDAO;
+    }
+
+    public ArrayList<GameData> listGames(String authToken) throws DataAccessException {
+        AuthData authData = authDao.findAuth(authToken);
+        if(authData != null) {
+            ArrayList<GameData> listOfGames = gameDAO.listGames();
+            return listOfGames;
+        }
+        else {
+            throw new DataAccessException("Error: unauthorized");
+        }
+    }
+
+    public int createGame(String authToken, String gameName) throws DataAccessException {
+        AuthData authData = authDao.findAuth(authToken);
+        int gameID = 1;
+        GameData gameData = new GameData(1, authData.username(), null, gameName, new ChessGame());
+        gameDAO.createGame(gameData);
+        return gameID;
     }
 
 }
